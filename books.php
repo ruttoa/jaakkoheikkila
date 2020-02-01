89a16349019b1d01e81850c2fdb3c475e56fe893
Janne Rättyä <janne.rattya@web-veistamo.fi>
2020-02-01 20:02:28 +0200

    Add popup gallery to books template.

--- books.php
+++ books.php
@@ -25,13 +25,61 @@ get_header(); ?>
 					));
 					$i = 1;
 					if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+							$book_gallery = get_field('book_gallery');
+							$book_text_left = get_field('book_text_left');
+							$book_text_right = get_field('book_text_right');
 					?>
 							<div id="book-<?php echo $i; ?>" class="book" data-aos="fade-up">
-								<a href="#" data-target="#" class="js-open-gallery">
+								<a href="#" data-target="#book-<?php echo $i; ?>-gallery" class="js-open-gallery">
 									<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/books/book-<?php echo $i; ?>.jpg" alt="" />
 									<h4 class="book__title"><?php the_title(); ?></h4>
-									<button class="open-gallery-link open-gallery-link--small js-open-gallery">Open gallery <span></span></button>
+									<button class="open-gallery-link open-gallery-link--small js-open-gallery" data-target="#book-<?php echo $i; ?>-gallery">Open gallery <span></span></button>
 								</a>
+							</div>
+							<div id="book-<?php echo $i; ?>-gallery" class="b-overlay b-overlay--book">
+								<div class="b-overlay__header">
+									<div class="container container--narrow">
+										<div class="b-overlay__site-title"><a href="<?php echo esc_url(home_url('/')); ?>">Jaakko Heikkilä</a></div>
+										<h3 class="b-overlay__gallery-title"><?php the_title(); ?></h3>
+										<div class="b-overlay__controls category-photographer">
+											<a href="#" class="close js-overlay-close">
+												<em class="line-1"></em>
+												<em class="line-2"></em>
+											</a>
+										</div>
+									</div>
+								</div>
+								<div class="b-overlay__text">
+									<div class="container container--narrow">
+										<div class="row">
+											<div class="col">
+												<?php echo $book_text_left; ?>
+											</div>
+											<div class="col">
+												<?php echo $book_text_right; ?>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div id="slider-grid" class="gallery-grid b-overlay__grid">
+									<div class="container container--narrow">
+										<div class="row">
+											<?php
+											if ($book_gallery) :
+												foreach ($book_gallery as $image) :
+											?>
+													<div class="col">
+														<img src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+													</div>
+												<?php
+												endforeach;
+											else :
+												?>
+												<p>No images in gallery.</p>
+											<?php endif; ?>
+										</div>
+									</div>
+								</div>
 							</div>
 					<?php
 							$i++;
@@ -61,9 +109,56 @@ get_header(); ?>
 					if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
 					?>
 							<div id="book-<?php echo $i; ?>" class="book" data-aos="fade-up">
-								<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/books/book-<?php echo $i; ?>.jpg" alt="" />
-								<h4 class="book__title"><?php the_title(); ?></h4>
-								<button class="open-gallery-link open-gallery-link--small js-open-gallery">Open gallery <span></span></button>
+								<a href="#" data-target="#book-<?php echo $i; ?>-gallery" class="js-open-gallery">
+									<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/books/book-<?php echo $i; ?>.jpg" alt="" />
+									<h4 class="book__title"><?php the_title(); ?></h4>
+									<button class="open-gallery-link open-gallery-link--small js-open-gallery" data-target="#book-<?php echo $i; ?>-gallery">Open gallery <span></span></button>
+								</a>
+							</div>
+							<div id="book-<?php echo $i; ?>-gallery" class="b-overlay b-overlay--book">
+								<div class="b-overlay__header">
+									<div class="container container--narrow">
+										<div class="b-overlay__site-title"><a href="<?php echo esc_url(home_url('/')); ?>">Jaakko Heikkilä</a></div>
+										<h3 class="b-overlay__gallery-title"><?php the_title(); ?></h3>
+										<div class="b-overlay__controls category-photographer">
+											<a href="#" class="close js-overlay-close">
+												<em class="line-1"></em>
+												<em class="line-2"></em>
+											</a>
+										</div>
+									</div>
+								</div>
+								<div class="b-overlay__text">
+									<div class="container container--narrow">
+										<div class="row">
+											<div class="col">
+												<?php echo $book_text_left; ?>
+											</div>
+											<div class="col">
+												<?php echo $book_text_right; ?>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div id="slider-grid" class="gallery-grid b-overlay__grid">
+									<div class="container container--narrow">
+										<div class="row">
+											<?php
+											if ($book_gallery) :
+												foreach ($book_gallery as $image) :
+											?>
+													<div class="col">
+														<img src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+													</div>
+												<?php
+												endforeach;
+											else :
+												?>
+												<p>No images in gallery.</p>
+											<?php endif; ?>
+										</div>
+									</div>
+								</div>
 							</div>
 					<?php
 							$i++;
