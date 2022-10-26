a21f3aa73bd437441459a1922cddc7dd68038a8f
Janne Rättyä <janne.rattya@web-veistamo.fi>
2022-10-26 19:49:42 +0300

    Add ACF fields for front page texts.

--- front-page.php
+++ front-page.php
@@ -3,9 +3,7 @@
 	<?php while (have_posts()) : the_post(); ?>
 		<section class="clearfix section section--intro">
 			<div class="container container--narrow">
-				<h1 data-aos="fade-up" data-aos-offset="0">I’ve been laying all day <br>
-					letting the light come <br>
-					into my room.</h1>
+				<h1 data-aos="fade-up" data-aos-offset="0"><?php the_field('title'); ?></h1>
 				<img data-aos="fade-up" data-aos-offset="0" data-aos-delay="500" src="<?php echo get_stylesheet_directory_uri(); ?>/img/home-1.jpg" alt="" />
 			</div>
 		</section>
@@ -16,11 +14,10 @@
 						<img data-aos="fade-right" src="<?php echo get_stylesheet_directory_uri(); ?>/img/home-2.jpg" alt="" />
 					</div>
 					<div class="col">
-						<h2 data-aos="fade-left">Beyond <br>
-							photography</h2>
+						<h2 data-aos="fade-left"><?php the_field('beyond_photography_title'); ?></h2>
 						<blockquote>
-							<p>Jaakko Heikkilä realizes a photocraphic art Beyond photography aesthetically in the Philosphical tradition of romanticism of Caspar David Friedrich or an enlightened Rationalism of Max Ernst and Salvador Dali. Yet at the same time his work contrawise, contradicts this classification. He moves us by touching the many wounds od globally oriented societes and cultures with his padst and current projects. </p>
-							<cite><strong>Andreas Vowinckel</strong>, Arthistorian, Cologne, Germany</cite>
+							<p><?php the_field('beyond_photography_quote'); ?></p>
+							<cite><?php the_field('beyond_photography_cite'); ?></cite>
 						</blockquote>
 					</div>
 				</div>
@@ -28,21 +25,21 @@
 		</section>
 		<section class="clearfix section section--projects">
 			<div class="container container--narrow">
-				<h2>Projects</h2>
+				<h2><?php the_field('projects_title'); ?></h2>
 				<div class="latest-projects">
 					<?php
-					$query = new WP_Query(array(
+					$project_query = new WP_Query(array(
 						'post_type'     => 'project',
 						'post_status'	=> 'publish',
 						'posts_per_page' => 3,
 					));
-					if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+					if ($project_query->have_posts()) : while ($project_query->have_posts()) : $project_query->the_post();
 							get_template_part('template-parts/content', 'project-home');
 						endwhile;
 					else :
 						get_template_part('template-parts/content', 'none');
-						wp_reset_postdata();
 					endif;
+					wp_reset_postdata();
 					?>
 				</div>
 				<a class="text-link text-link--big" href="<?php echo get_post_type_archive_link('project'); ?>">explore all</a>
@@ -52,8 +49,8 @@
 			<div class="container container--narrow">
 				<div class="books-image">
 					<div class="books-image__inner">
-						<h2>Books</h2>
-						<p>There is no other way, the way worward is a mystery Dreams are movement, the future is in our blood Titian knew how to paint black, a man rises out of blackness.</p>
+						<h2><?php the_field('books_title'); ?></h2>
+						<p><?php the_field('books_quote'); ?></p>
 						<a href="/books" class="text-link text-link--small">explore more</a>
 					</div>
 				</div>
@@ -61,10 +58,10 @@
 		</section>
 		<section class="clearfix section section--gallery">
 			<div class="container container--narrow">
-				<h2>Gallery</h2>
+				<h2><?php the_field('gallery_title'); ?></h2>
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home-4.jpg" alt="" />
 				<div class="gallery-text">
-					<p>You are able to order gallery images. I print them to the beautiful Hahnemuehle Photo Rag paper and sign them. </p>
+					<p><?php the_field('gallery_description'); ?></p>
 					<a href="/gallery" class="text-link text-link--big">explore more</a>
 				</div>
 			</div>
@@ -76,20 +73,20 @@
 		</section>
 		<section class="clearfix section section--news">
 			<div class="container">
-				<h2>News</h2>
+				<h2><?php the_field('news_title'); ?></h2>
 				<div class="latest-news row three-col">
 					<?php
-					$query = new WP_Query(array(
+					$news_query = new WP_Query(array(
 						'post_type'     => 'post',
 						'posts_per_page' => 6,
 					));
-					if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+					if ($news_query->have_posts()) : while ($news_query->have_posts()) : $news_query->the_post();
 							get_template_part('template-parts/content', get_post_format());
 						endwhile;
 					else :
 						get_template_part('template-parts/content', 'none');
-						wp_reset_postdata();
 					endif;
+					wp_reset_postdata();
 					?>
 				</div>
 				<a class="text-link" href="<?php echo get_post_type_archive_link( 'post' ); ?>">All news</a>
